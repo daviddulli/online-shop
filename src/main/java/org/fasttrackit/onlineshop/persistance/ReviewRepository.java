fa2080bf4d9e609eeeaaeaca42379866cb986c35
daviddulli <daviddulli2@gmail.com>
2020-03-25 20:21:56 +0200

    added product reviews (studied many to one relationship)

---
+++ src/main/java/org/fasttrackit/onlineshop/persistance/ReviewRepository.java
@@ -0,0 +1,16 @@
+package org.fasttrackit.onlineshop.persistance;
+
+import org.fasttrackit.onlineshop.domain.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    //finding reviews based by nested property(id property of product)
+    Page<Review> findByProductId(long productId, Pageable pageable);
+
+
+
+    
+}
